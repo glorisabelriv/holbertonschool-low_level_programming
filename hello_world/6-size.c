c0b0a475e831932538e58743224a4e48fef80d1f
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-19 18:58:35 -0400

    program that prints the size of various

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,18 +5,18 @@
  *
  */
 
-int main(void) 
+int main(void)
 {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-    // sizeof evaluates the size of a variable
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    
-    return 0;
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
+	return (0);
 }
