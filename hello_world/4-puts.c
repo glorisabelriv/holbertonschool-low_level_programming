ffed4e373ad5610e5e776eee4d9baffbc938a003
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-19 18:27:07 -0400

    write a c program that prints exactly

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+* main - entry point
+* * Return: program should end with the value 0
+*/
 
 int main(void)
 {
-  puts("\n"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
