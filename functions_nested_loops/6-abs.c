55bab1d98f6c76ee4065bb3941a9d6e6d5f2f265
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-22 19:25:04 -0400

    function that computes the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value of a integer
+ *@c: Number
+ *Return: value
+ **/
+int _abs(int c)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
