856392ba50db67ee9f674894494b84a2a5b5a172
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-22 15:58:08 -0400

    write a function that prints the sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *@n: variable
+ *Return: 1 if is greater that zero else 0
+ */
+int print_sign(int n)
+{
+	if (n > 0 '+' ; && n < 0 '-')
+		return (1);
+	else
+		return (0);
+}
