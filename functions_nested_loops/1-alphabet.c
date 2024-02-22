55811044424493037d3f97917cdbd359fa6d5d1e
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-21 20:05:23 -0400

    funtion that prints the alphabetic lowercase

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
- * main - entry point
+ * print_alphabet - lower case
  *
  * Return: Always 0
  **/
-int main(void)
+void print_alphabet(void)
 {
 	int x;
 for (x = 'a'; x <= 'z' ; x++)
 	_putchar(x);
 	_putchar('\n');
-	return (0);
 }
