6af1b6e9f07ec384aac550359098aacf7f834332
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-21 20:30:28 -0400

    print 10 times alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print_alphabet_x10 void
+ *
+ * Return: void
+ **/
+
+void print_alphabet_x10(void)
+{
+	print_alphabet_x10('abc..');
+	  _putchar('abc');
+		  _putchar('\n');
+}
