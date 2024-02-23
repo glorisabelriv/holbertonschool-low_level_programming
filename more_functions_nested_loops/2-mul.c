ec17d8eb74cfb4d006d4abfafe8a7d0b6fd4ff32
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@MacBook-Air-de-Glorisabel.local>
2024-02-22 23:22:53 -0400

    function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - that multiplies two intgers
+ *@a: variable
+ *@b: variable
+ *Return: result
+ **/
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
