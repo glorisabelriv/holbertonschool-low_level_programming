7868f86efd9ce0e979192f9f38ceb0b5affd9c45
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-03-13 22:22:31 -0400

    function that searches for a integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,15 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	int i;
 
-	if (size <= 0)
+	if (array && cmp)
 	{
-		return (-1);
-	}
-
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-		{
 			return (i);
 		}
 	}
