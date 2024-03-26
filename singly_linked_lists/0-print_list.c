d1df62bb8dc079edef406b9c430fca1e293c77a9
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-03-25 22:15:31 -0400

    arregle betty codig style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@ size_t print_list(const list_t *h)
 	size_t nod = 0;
 
 	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 
