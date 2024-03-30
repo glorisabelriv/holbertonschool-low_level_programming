374a618e73af7371090865349faa794b2b48aebc
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-03-30 13:59:25 -0400

    include the all prototype

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 
