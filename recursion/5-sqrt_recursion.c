8320eb67ad8ce508eaf54d573fbacb27efb694e1
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-10 21:02:00 -0400

    function that returns the natural square root of a number.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *@r: variable
+ * @n: number
+ * Return: natural square root or -1
+ */
+int square(int r, int n);
+
+int _sqrt_recursion(int n)
+{
+	return (square(0, n));
+}
+/**
+ * square - function created
+ * @r: variable
+ * @n: variab;e
+ * Return: natural square root
+ */
+
+int square(int r, int n)
+{
+	int sr = r * r;
+
+	if (sr < n)
+	{
+		return (square(r + 1, n));
+	}
+	else if (sr > n)
+	{
+		return (-1);
+	}
+	return (r);
+}
