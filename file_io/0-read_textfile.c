37c91c63caf90ba1f1598a4e5333561210b5de8d
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-18 17:59:41 -0400

    function that reads a text file and prints it to the POSIX standard output.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,36 +8,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t i, r;
-	char *fp;
-
-	fp = malloc(sizeof(char) * letters);
-	
-	if (fp == NULL)
-	{
-		return (0);
-	}
+	int fd;
+	ssize_t read_count, write_count;
+	char *buffer;
 
 	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-	   return (0);
-	}
-
-
-	i = open(filename, O_RDONLY);
-
-	if (i == -1)
-	{
-		free(fp);
+		close(fd);
 		return (0);
 	}
-	r = read(i, fp, letters);
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	fp[letters + 1] = '\0';
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || write_count != read_count)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	 write(STDOUT_FILENO, fp, r);
-
-	close(i);
-
-	return (r);
+		free(buffer);
+		close(fd);
+		return (write_count);
 }
