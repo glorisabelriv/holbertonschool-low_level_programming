09974d0db58efb00829c39fae52f87fda56ab70e
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-18 20:00:42 -0400

    program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,32 +1,68 @@
 #include "main.h"
+
 /**
  * main - copies the content of a file into another
- *@argc: fisrst argument
- *@argv: 2nd argument 
- *Return: cont of file
+ * @argc: fisrst argument
+ * @argv: 2nd argument
+ * Return: cont of file
  */
-int main()
-{
-	FILE *fp1, *fp2;
-	char c;
 
-	fp1 = fopen("file_from", "r");
-	if (fp1 == NULL)
-	{
-		printf("Usage: cp file_from file_to\n");
-			exit(97);
-	}
-	fp2 = fopen("file_to", "w");
-	if (fp2 == NULL)
-	{
-		printf("Error: Can't read from file\n");
-			exit(98);
-	}
-	while ((c = fgetc(fp1)) != EOF)
-	{
-		fputc(c, fp2);
-	}
-	fclose(fp1);
-	fclose(fp2);
-	return (0);
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to;
+char buffer[BUFFER_SIZE];
+ssize_t bytes_read, bytes_written;
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: cant read from file %s\n", argv[1]);
+exit(98);
+}
+
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(fd_from);
+exit(99);
+}
+
+while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written != bytes_read)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(fd_from);
+close(fd_to);
+exit(99);
+}
+}
+
+if (bytes_read == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+if (close(fd_from) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+exit(100);
+}
+
+if (close(fd_to) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+exit(100);
+}
+
+return (0);
 }
